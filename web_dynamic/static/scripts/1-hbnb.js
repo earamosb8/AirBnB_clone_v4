9dcdb842f4c8dcf22f10b99e31df8f3fb203cd0b
somarae8 <somarae8@gmail.com>
2020-09-27 15:58:33 -0500

    task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    let check_list = {};
+    $('li input[type="checkbox"]').change(function () {
+    if ($(this).is(":checked")){
+      check_list[$(this).data("id")] = $(this).data("name")
+    } else {
+      delete list_check[$(this).data("id")]
+  }
+      const values = Object.values(check_list)
+      const addlist = values.join(', ')
+      const short = addlist.slice(0, 25)
+      $('.amenities h4').text(short + '...')
+      if (values.length === 0) $('.amenities h4').html('&nbsp;');
+  })})
+});
